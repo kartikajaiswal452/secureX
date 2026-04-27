5345603ba3ca5c1025d40a89f381248964cd4350
Kartik Jaiswal <kartikajaiswal452@example.com>
2026-04-27 10:50:20 +0530

    fix userRoutes path

--- Backend/routes/userroutes.js
+++ Backend/routes/userroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
-const { uploadProfilePic } = require("../controllers/userController");
+const { uploadProfilePic } = require("../controllers/usercontroller");
 
 router.post("/upload-profile", auth, upload.single("image"), uploadProfilePic);
 
